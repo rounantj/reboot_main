7a28be664ca92f18b58ff3f7c2e31c6542fb5f54
RONAN RODRIGUES <rouna@BRLNHN015578.weg.net>
2022-06-05 14:43:45 -0300

    first

--- database.js
+++ database.js
@@ -1,19 +1,18 @@
 var express = require('express')
 var app = express();
-const handlebars = require('express-handlebars');
+ 
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const { COPYFILE_EXCL } = fs.constants;
-var sqlite = require('sqlite-sync');
+ 
 
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.static('public'));
-app.engine('handlebars', handlebars({defaultLayout:'main'}))
-app.set('view engine', 'handlebars')
-
+ 
+ 
 const cors = require('cors');
 app.use(express.json())
 require('dotenv').config()
