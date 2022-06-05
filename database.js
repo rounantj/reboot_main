6d1fc81e05eda26bcde88dc2490cf78575001350
RONAN RODRIGUES <rouna@BRLNHN015578.weg.net>
2022-06-05 18:02:27 -0300

    first

--- database.js
+++ database.js
@@ -51,31 +51,7 @@ async function sqlQueryJson(query) {
         return {
             "error": e
         }
-
-    //     setTimeout(function () {
-    //         // Listen for the 'exit' event. 
-    //         // This is emitted when our app exits.
-    //         process.on("exit", function () {
-    // ​
-    //           //  Resolve the `child_process` module, and `spawn` 
-    //           //  a new process.
-    //           //  The `child_process` module lets us
-    //           //  access OS functionalities by running any bash command.`.
-    //           require("child_process")
-    //             .spawn(
-    //               process.argv.shift(),
-    //               process.argv,
-    //               {
-    //                 cwd: process.cwd(),
-    //                 detached: true,
-    //                 stdio: "inherit"
-    //               }
-    //             );
-              
-    //         });
-    //         process.exit();
-    //     }, 1000);
-   
+ 
     }
 }
  
