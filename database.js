e562a7c0da37c0f43ce1345a8c7f16d1aa6f6c53
RONAN RODRIGUES <rouna@BRLNHN015578.weg.net>
2022-06-05 17:28:01 -0300

    first

--- database.js
+++ database.js
@@ -18,7 +18,7 @@ app.use(express.json())
 require('dotenv').config()
 var mongoose = require('mongoose')
 //var strConn = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0'
- 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
